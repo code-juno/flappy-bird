eba580b347e0b0d9c5503711e5bde1dd2131616d
Brian Alldred <bri_alldred@live.co.uk>
2024-02-23 21:39:39 +0000

    pipes added

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
 import React from "react";
-import { Canvas, Image } from "@shopify/react-native-skia";
-import { useWindowDimensions } from "react-native"; 
-import { useImage } from "@shopify/react-native-skia";
+import { Canvas, Image, useImage } from "@shopify/react-native-skia";
+import { useWindowDimensions } from "react-native";
+
+const BIRD_WIDTH = 64;
+const BIRD_HEIGHT = 48;
+const PIPE_WIDTH = 104;
+const PIPE_HEIGHT = 640;
 
 const App = () => {
   const { width, height } = useWindowDimensions();
-  const background = useImage(require("./assets/sprites/background-day.png"));
+  const bg = useImage(require("./assets/sprites/background-day.png"));
+  const bird = useImage(require("./assets/sprites/yellowbird-upflap.png"));
+  const pipeBottom = useImage(require("./assets/sprites/pipe-green.png"));
+  const pipeTop = useImage(require("./assets/sprites/pipe-green-top.png"));
+  const pipeOffset = 0;
 
   return (
     <Canvas style={{ width, height }}>
-      <Image image={background} fit="cover" x={0} y={0} width={width} height={height} />
+      <Image image={bg} fit="cover" width={width} height={height} />
+      <Image image={pipeTop} x={width / 2} y={pipeOffset - PIPE_HEIGHT / 2} width={PIPE_WIDTH} height={PIPE_HEIGHT} />
+      <Image image={pipeBottom} x={width / 2} y={pipeOffset + height - PIPE_HEIGHT / 2} width={PIPE_WIDTH} height={PIPE_HEIGHT} />
+      <Image image={bird} x={width / 4} y={(height - BIRD_HEIGHT) / 2} width={BIRD_WIDTH} height={BIRD_HEIGHT} />
+
+
     </Canvas>
   );
 };
- 
+
 export default App;
